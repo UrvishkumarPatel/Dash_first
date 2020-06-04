7e561c9535e6bb69ffd1155137c103a280c76680
Urvishkumar Patel <patel.urvishkumar@iitgn.ac.in>
2020-06-05 00:33:35 +0530

    Graph added

--- Dash.py
+++ Dash.py
@@ -6,8 +6,9 @@ from dash.dependencies import Input, Output
 
 import dash_table
 
-import config
-
+# import config
+from config import  *
+from Graph import *
 
 ###################
 import dash_bootstrap_components as dbc
@@ -23,9 +24,11 @@ colors = {
     'text': '#00AFE9'}
 
 BS = "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-# available_themes =  ['CERULEAN', 'COSMO', 'CYBORG', 'DARKLY', 'FLATLY', 'JOURNAL', 'LITERA', 'LUMEN', 'LUX', 'MATERIA', 'MINTY', 'PULSE', 'SANDSTONE', 'SIMPLEX', 'SKETCHY', 'SLATE', 'SOLAR', 'SPACELAB', 'SUPERHERO', 'UNITED', 'YETI']
+# available_themes =  ['CERULEAN', 'COSMO', 'CYBORG', 'DARKLY', 'FLATLY', 'JOURNAL', 
+#                       'LITERA', 'LUMEN', 'LUX', 'MATERIA', 'MINTY', 'PULSE', 'SANDSTONE', 
+#                       'SIMPLEX', 'SKETCHY', 'SLATE', 'SOLAR', 'SPACELAB', 'SUPERHERO', 'UNITED', 'YETI'
+#                       ]
 stylesheets = [dbc.themes.FLATLY]
-logo_src = config.logo_src
 options = {'Country': ['India','USA','China'],'Region':['Americas','Europe & Middle East','Pacific'],'DM-EM Flag': ['DM','EM'],'ACWI':['ACWI']} 
 
 
@@ -36,14 +39,18 @@ app = dash.Dash(__name__, external_stylesheets = stylesheets)
 # app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 # style={'backgroundColor': colors['background']},
 
+#########################
+
+
+
 app.layout = html.Div( children=[
 
     html.Div([
         html.Img(
         src = logo_src,
         style={
-            'height': '10%',
-            'width': '10%',
+            'height': '12%',
+            'width': '12%',
             'float': 'right',
             'position': 'relative',
         }
@@ -53,30 +60,88 @@ app.layout = html.Div( children=[
     html.Div([
         html.H1(children='MSCI Rebalance'), 
         html.H5(children='Carry out a rebalance for any country/region/DM-EM Market/ACWI')
-        ], style={'margin-top':20,'textAlign': 'center','position': 'relative', 'color': colors['text']},className="m-5"),
+        ], style={'textAlign': 'center','position': 'relative', 'color': colors['text']}),
     
     html.Hr(),
+    # html.Div(
+    #     [
+    #     html.Div(
+    #         [html.Div([html.Label(children = 'Date of Used data',style = {'color': colors['text'],'margin-left':20} ),], className="row"),
+    #         html.Div([dcc.DatePickerSingle(
+    #             id='date-picker-single',
+    #             date= dt.date(2020,5,24),
+    #             style = {'margin-left':20}
+    #         )], className="row")], 
+    #         className = "col-3", style={'float': 'left','textAlign': 'left','position': 'relative'}),
+        
+    #     html.Div(
+    #         [html.Label(children = 'Select country',style = {'color': colors['text']} ),
+    #         dcc.Dropdown(id = 'countries-dropdown',
+    #             placeholder="Type something...",
+    #             options=[{'label':'United','value':'US'},{'label':'Canada','value':'CA'}],
+    #             value='MTL'
+    #         )],className = "col-6", style={'float': 'right','position': 'relative','margin-left':20, 'margin-right':20}),
+        
+        
+    #     html.Div([
+    #         dbc.Button('Submit', id='button', style = {'textAlign': 'center','float': 'center','margin-top':15},),
+    #     ], className="col")
+        
+    #     ], className = "row",),
+        
     html.Div(
         [
         html.Div(
-            [html.Div([html.Label(children = 'Date of Used data',style = {'color': colors['text'],'margin-left':20} ),], className="row"),
-            html.Div([dcc.DatePickerSingle(
+            [html.Div([html.Label(children = 'Date of Used data',style = {'color': colors['text'],'margin-left':20} ),])], 
+            className = "col-3", style={'float': 'left','textAlign': 'left','position': 'relative'}),
+        
+        html.Div(
+            [html.Label(children = 'Select country',style = {'color': colors['text']} ),
+            ],className = "col-6", style={'float': 'right','position': 'relative','margin-left':20, 'margin-right':20}),
+        
+        
+        html.Div([
+           ], className="col")
+        
+        ], className = "row",),
+        
+        
+    html.Div(
+        [
+        html.Div(
+            [html.Div([dcc.DatePickerSingle(
                 id='date-picker-single',
                 date= dt.date(2020,5,24),
                 style = {'margin-left':20}
-            )], className="row")
-                
-                
-            ], 
-            className = "col", style={'float': 'left','textAlign': 'left','position': 'relative'}),
+            )])], 
+            className = "col-3", style={'float': 'left','textAlign': 'left','position': 'relative'}),
+        
         html.Div(
-            [html.Label(children = 'Select country',style = {'color': colors['text']} ),
-            dcc.Dropdown(id = 'countries-dropdown',
+            [dcc.Dropdown(id = 'countries-dropdown',
                 placeholder="Type something...",
                 options=[{'label':'United','value':'US'},{'label':'Canada','value':'CA'}],
                 value='MTL'
-            )],className = "col-8", style={'float': 'right','position': 'relative','margin-left':20})
-        ], className = "row", style = {'margin-top': 1}),
+            )],className = "col-6", style={'float': 'right','position': 'relative','margin-left':20, 'margin-right':20}),
+        
+        
+        html.Div([
+
+            dbc.Button('Submit', id='button', style = {'textAlign': 'center','float': 'center'},),
+            
+            # just in case you want to use
+            dcc.ConfirmDialogProvider(
+                children=html.Button(
+                    'Submit',
+                ),
+                id='danger-danger',
+                message='Danger danger! Are you sure you want to continue?'
+            ),
+            # ##############################
+
+            
+        ], className="col")
+        
+        ], className = "row",),
 
     ######################################################################### 
 
@@ -84,46 +149,84 @@ app.layout = html.Div( children=[
     html.Div([
         html.Div(
             [html.Label(children = 'Rebalance Type',style = {'color': colors['text']}),
-            dcc.RadioItems(
-                id = 'radio',
-                options=[
-                    {'label': 'Semi Annual Review (May & Nov)', 'value': 'SAIR'},
-                    {'label': 'Quarter Review (Feb & Aug)', 'value': 'QIR'}
-                ],
-                labelStyle={'display': 'inline-block', 'margin-right':50},
-                value=['SAIR','QIR']
-            )] , style={'float': 'left','position': 'relative','margin-left':20}),
+            ] , style={'float': 'left','position': 'relative','margin-left':20}, className="col"),
         
-        ], className = "row", style = {'margin-bottom': 1}),
+        ], className = "row", ),
         
 
 
     #########################################
+    # html.Br(),
+    # html.Div([
+       
+    #     # html.Hr(),
+    #     html.Div(
+    #         [html.Div('Click below to generate the results',style = {'color': colors['text']}),
+    #         html.Button('Submit', id='button', style = {'textAlign': 'center','color': colors['text'],'margin-top':15},
+    #         )], style={'textAlign': 'center','position': 'relative'})
+    #     ], className = "row justify-content-center" , style={'float': 'center','position': 'relative', 'color': colors['text']}),
     html.Hr(),
     html.Div([
-       
-        # html.Hr(),
-        html.Div(
-            [html.Div('Click below to generate the results',style = {'color': colors['text']}),
-            html.Button('Submit', id='button', style = {'textAlign': 'center','color': colors['text'],'margin-top':15},
-            )], style={'textAlign': 'center','position': 'relative'})
-        ], className = "row justify-content-center" , style={'float': 'center','position': 'relative', 'color': colors['text']}),
+        html.Div([
+            # dbc.Table(id='table-1').from_dataframe(df, striped=True, bordered=True, hover=True),
+            dash_table.DataTable(id='table-1'),
+        ], className="col-4 ml-3 mr-3"),
+        html.Div([
+            dash_table.DataTable(id='table-2'),
+        ], className="col-4 ml-3 mr-3"),
+        
+        ],className = "row justify-content-center" , style={'position': 'relative'}),
 
     html.Br(),
     html.Div([
         html.Div([
-            dash_table.DataTable(id='table'),
-        ], className="col-6")
-        
-        ],className = "row justify-content-center" , style={'position': 'relative'}),
+            html.Div([
+                dcc.Graph(
+                    figure = fig,
+                    style={'height': 300},
+                    id='my-graph-1'
+                )  
+            ],className="col"),
+            html.Div([
+                dcc.Graph(
+                    figure = fig,
+                    style={'height': 300},
+                    id='my-graph-2'
+                )  
+            ],className="col"),
+
+        ],className="row justify-content-center"),
+
+
+        html.Div([
+            html.Div([
+                dcc.Graph(
+                    figure = fig,
+                    style={'height': 300},
+                    id='my-graph-3'
+                )  
+            ],className="col"),
+            html.Div([
+                dcc.Graph(
+                    figure = fig,
+                    style={'height': 300},
+                    id='my-graph-4'
+                )  
+            ],className="col"),
+
+        ],className="row justify-content-center"),
+
+    ], className="row justify-content-center"),
     
     
     # html.Div([form])
     # html.P(id = 'date_out')
 
     
-], className = "m-5")
+], className = "m-3")
 
+
+# ,style={"background-image": 'url("/assets/Background.PNG")'
 
 
 
@@ -135,11 +238,24 @@ app.layout = html.Div( children=[
 
 
 
-@app.callback([Output("table", "data"), Output('table', 'columns')],
-    [Input('countries-dropdown', 'value'), Input('radio', 'value')])
+@app.callback([Output("table-1", "data"), Output('table-1', 'columns')],
+    [Input('countries-dropdown', 'value')])
 
-def update_table(selected_cont, radio):
-    if radio == 'SAIR':    
+def update_table(selected_cont):
+    if False:    
+        temp = df[df['Country'] == selected_cont]
+    else:
+        temp = df
+    # temp = df
+    column = [{"name": i, "id": i} for i in temp.columns]
+    return temp.to_dict('records'), column
+
+    
+@app.callback([Output("table-2", "data"), Output('table-2', 'columns')],
+    [Input('countries-dropdown', 'value')])
+
+def update_table(selected_cont):
+    if False:    
         temp = df[df['Country'] == selected_cont]
     else:
         temp = df
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 themes =  ['CERULEAN', 'COSMO', 'CYBORG', 'DARKLY', 'FLATLY', 'JOURNAL', 'LITERA', 'LUMEN', 'LUX', 'MATERIA', 'MINTY', 'PULSE', 'SANDSTONE', 'SIMPLEX', 'SKETCHY', 'SLATE', 'SOLAR', 'SPACELAB', 'SUPERHERO', 'UNITED', 'YETI']
 
-logo_src = "https://1000logos.net/wp-content/uploads/2016/10/Barclays-Logo.png"
+# logo_src = "https://1000logos.net/wp-content/uploads/2016/10/Barclays-Logo.png"
+logo_src = "/assets/Background.PNG"
